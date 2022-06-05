76728fd0daedf7b1c8bf6b39be1e6135b267a559
Michael Gene Brockus <michaelbrockus@gmail.com>
2022-06-05 16:54:56 -0500

    add command class clean

---
+++ code/mesoncli/clean.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+#
+# Troglobyte stdlib:
+# author: Michael Gene Brockus
+# Gmail: <mail: michaelbrockus@gmail.com>
+#
+from pathlib import Path
+import subprocess
+
+
+'''Meson clean command wrapper class'''
+class MesonClean:
+    '''Constructors are used to initialize the
+    object’s state. The task of constructors is to
+    initialize(assign values) to the data members
+    of the class when an object of class is created.
+    
+    In this case anything for the --clean switch from
+    the meson compile command.
+    '''
+    def __init__(self, builddir: Path):
+        self._builddir: Path = builddir
+        super().__init__()
+    # end of method
+
+    '''Should run the process of the current command
+    which in this case is to clean the build directory
+    with the --clean switch from the compile command.
+    '''
+    def run(self, args: list = []):
+        run_cmd = ['meson', 'compile', '--clean', '-C', str(self._builddir)]
+        process = subprocess.Popen(run_cmd, encoding='utf8', stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        return process.communicate()[0]
+    # end of method
+
+# end class
